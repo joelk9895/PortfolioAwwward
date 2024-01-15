88c9bb7feb6a2d86c02d67c9ab8a1a343fc08e69
Joel K George <joelk9895@gmail.com>
2024-01-15 19:42:12 +0530

    Hero

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <main>
       <div className="absolute top-0 left-0 w-full h-20 bg-white z-10 flex items-center justify-center">
-        <h2 className="h-fit text-3xl font-mono">welcome</h2>
+        <h2 className="h-fit text-3xl">welcome</h2>
       </div>
       <ParticleCanvas />
       <div className="relative hero">
         <header>
           <h1>Joel K George</h1>
         </header>
-        <p className="font-mono">
+        <p>
           Full Stack Dev
           <br />
           ML Enthusiast
